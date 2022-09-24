147a5d2365ad338240dda9b190730cc897cbafd2
arka <arkamallick30@gmail.com>
2022-09-24 03:38:08 +0200

    introduced lifecycle methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,23 +3,29 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMessage: '' };
 
-        this.state = { lat: null };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({lat : position.coords.latitude});
-            }
+            (position) => this.setState({lat : position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
         );
     }
 
     render( ) {
-     
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div> Error: {this.state.errorMessage}</div>;
+        }
 
-        return <div> Latitude: {this.state.lat}</div>
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div> Latitude: {this.state.lat}</div>;
+        }
+
+        return <div>loading!...</div>;
+
     }
+
+    
 }
 
 ReactDOM.render(
