7aa2687e4ae45bc305d7b375de0858e9790b2463
Arka Mallick <arkamallick30@gmail.com>
2023-02-12 13:19:54 +0100

    some lambda fun

---
+++ javaInterview/lambdaFuture/lambdaFuture/src/test/java/dr/sattlers/bar/lambdaFuture/LambdaFutureApplicationTests.java
@@ -0,0 +1,13 @@
+package dr.sattlers.bar.lambdaFuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaFutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
